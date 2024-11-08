a664d61426663fc67d9cf1c28252adcd41102880
Tw <tw19881113@gmail.com>
2024-11-08 11:09:06 +0800

    init
    Signed-off-by: Tw <tw19881113@gmail.com>

---
+++ settings.cmake
@@ -0,0 +1,6 @@
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fno-stack-protector")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fno-stack-protector")
+
+set(LibSel4FunctionAttributes public)
+set(KernelVerificationBuild OFF)
+set(KernelPrinting ON)
---
+++ zig_toolchain.cmake
@@ -0,0 +1,2 @@
+set(CMAKE_C_COMPILER zig cc -target ${ZIG_TARGET})
+set(CMAKE_CXX_COMPILER zig c++ -target ${ZIG_TARGET})
